fb9efed91119d683cb603da22fd4d52d9010d197
smackeem <smackeem@outlook.com>
2023-11-01 19:49:22 -0400

    Change max Level back to 15.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const INIT_STATE = {
   highScore: 0,
 };
 const colorsArr = ["green", "blue", "red", "yellow"];
-const maxLevel = 5;
+const maxLevel = 15;
 
 /*----- state variables -----*/
 let state;
